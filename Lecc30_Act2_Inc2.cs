583be835ab41e55d82bbde01da20e164b3514560
2014000148-DiegoTrujillo <d.trujillo.5adb@gmail.com>
2026-06-16 17:29:38 -0600

    Create Lecc30_Act2_Inc2.cs

---
+++ Lecc30_Act2_Inc2.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("DATOS ACADEMICOS\nNombre: Diego Trujillo" +
+             "\nGrado:IV C" +
+             "\nClave: 31");
+
+        int[] numerosEnteros = new int[8];
+        int numeroMayor = 0;
+        int posicionMayor = 0;
+
+        for (int indice = 0; indice < 8; indice++)
+        {
+            Console.Write("Ingrese el número " + (indice + 1) + ": ");
+            numerosEnteros[indice] = Convert.ToInt32(Console.ReadLine());
+
+            if (indice == 0)
+            {
+                numeroMayor = numerosEnteros[indice];
+                posicionMayor = indice + 1;
+            }
+            else
+            {
+                if (numerosEnteros[indice] > numeroMayor)
+                {
+                    numeroMayor = numerosEnteros[indice];
+                    posicionMayor = indice + 1;
+                }
+            }
+        }
+
+        Console.WriteLine("El número mayor es: " + numeroMayor);
+        Console.WriteLine("Se encuentra en la posición: " + posicionMayor);
+    }
+}
